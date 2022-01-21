2d8ce6161b94fbf23ba93af4e0765fca16e6042b
tokyosuperch <supachan@sfc.wide.ad.jp>
2022-01-22 00:41:24 +0900

    NTP experiment

--- gpioout.c
+++ gpioout.c
@@ -9,13 +9,10 @@
 #define OUT_PORT 21
 
 struct timespec out_ts;
-struct tm out_tm;
-struct timespec offset;
 
 void gettime_out() {
 	clock_gettime(CLOCK_REALTIME, &out_ts);  //時刻の取得
-	localtime_r( &out_ts.tv_sec, &out_tm);
-	printf("%ld.%09ld\n",ts.tv_sec, out_ts.tv_nsec);
+	printf("%ld.%09ld\n",out_ts.tv_sec, out_ts.tv_nsec);
 }
 
 int main() {
@@ -28,12 +25,11 @@ int main() {
 		perror("Failed to Set OUTPUT mode");
 		return 1;
 	}
-	while(1) {
-		gpioWrite(OUT_PORT, HIGH);
-		usleep(1000000 - 1000);
-		gpioWrite(OUT_PORT, LOW);
-		gettime_out();
-		usleep(1000);
-	}
+	gpioWrite(OUT_PORT, HIGH);
+	usleep(1000);
+	gpioWrite(OUT_PORT, LOW);
+	gettime_out();
+	usleep(1000);
+	gpioWrite(OUT_PORT, HIGH);
 	gpioTerminate();
 }
--- loop.sh
+++ loop.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
 SLEEP_SEC=3500
-LOOP_MAX=168
+HALT_SEC=1
+LOOP_MAX=100
 
-COUNT=0
-while [ $COUNT -lt $LOOP_MAX ]
+while :
 do
-  
-  ./client.elf >> result0119.csv
-  
-  sleep ${SLEEP_SEC}
-  COUNT=`expr ${COUNT} + 1`
+	COUNT=0
+	while [ $COUNT -lt $LOOP_MAX ]
+	do
+		ntpdate -p 1 -b supachan.sfc.wide.ad.jp
+		if [ $? -ne 0 ]; then
+			COUNT=`expr ${COUNT}`
+		else
+			./gpioout.elf >> gpio_ntp_out.csv
+			sleep ${HALT_SEC}
+			COUNT=`expr ${COUNT} + 1`
+		fi
+	done
+	sleep ${SLEEP_SEC}
 done
