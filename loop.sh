683e1414254a32416c1099b98228df4eb8b9d88c
tokyosuperch <supachan@sfc.wide.ad.jp>
2022-01-23 18:08:11 +0900

    cronでの定期実行に変更

--- loop.sh
+++ loop.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
 
-SLEEP_SEC=3500
 HALT_SEC=1
 LOOP_MAX=100
 
-while :
+cd /home/supachan/offsetsync
+COUNT=0
+while [ $COUNT -lt $LOOP_MAX ]
 do
-	COUNT=0
-	while [ $COUNT -lt $LOOP_MAX ]
-	do
-		ntpdate -p 1 -b supachan.sfc.wide.ad.jp
-		if [ $? -ne 0 ]; then
-			COUNT=`expr ${COUNT}`
-		else
-			./gpioout.elf >> gpio_ntp_out.csv
-			sleep ${HALT_SEC}
-			COUNT=`expr ${COUNT} + 1`
-		fi
-	done
-	sleep ${SLEEP_SEC}
+	/usr/sbin/ntpdate -p 1 -b supachan.sfc.wide.ad.jp
+	if [ $? -ne 0 ]; then
+		COUNT=`expr ${COUNT}`
+	else
+		./gpioout.elf >> gpio_ntp_out.csv
+		sleep ${HALT_SEC}
+		COUNT=`expr ${COUNT} + 1`
+	fi
 done
+
